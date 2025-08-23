74c26ab091f613bea6f2a2a165a364cb33c7ed1e
graff012 <sobirjonovnuriddin43@gmail.com>
2025-08-23 08:34:59 +0500

    added logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -156,7 +156,7 @@ const Header = ({ searchTerm, setSearchTerm }: HeaderProps) => {
           <div className="hidden md:flex justify-between items-center">
             <Link to={"/"}>
               <h1 className="title text-4xl font-semibold text-white">
-                <img src={mainLogo} alt="Logo" />
+                <img src='/rozoviysad-logo-colored.svg' alt="Logo" />
               </h1>
             </Link>
 
